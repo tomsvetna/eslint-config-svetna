5ed6c7ac8ebcd01212f84faf1b3b056d200c63f7
Tomáš Světnický <tomas.svetnicky@siemens.com>
2021-10-30 19:56:49 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['airbnb', 'plugin:react/recommended', 'prettier'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+        'prettier/prettier': 'error',
+        'react/jsx-props-no-spreading': 'off',
+        'react/no-unescaped-entities': 'off',
+    },
+}
